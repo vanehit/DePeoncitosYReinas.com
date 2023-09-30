566ef30a80070b1858d48317198ad14efb3b2300
vanesa paola Soria <vanesasoria.com@gmail.com>
2023-09-29 23:24:55 -0300

    arreglando el front

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
+
 
 const app = express();
 
 //middlewares
 app.use(express.json());
 
+
 // Configuración de EJS como motor de plantillas
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, ''));
+app.set('views', path.join(__dirname, 'src', 'views'));
+
 
 
 // Middleware para servir archivos estáticos desde la carpeta "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// Rutas
-app.get('/', (req, res) => {
-  const featuredPosts = [
-    { title: 'Título 1', content: 'Contenido 1' },
-    { title: 'Título 2', content: 'Contenido 2' },
-    
-  ];
-  res.render('./src/views/pages/home.ejs', { featuredPosts });
+// Ruta para la página de inicio
+app.get('/', async (req, res) => {
+  try {
+    // Realiza la solicitud Axios para obtener las publicaciones
+    const response = await axios.get('http://localhost:5000/posts'); 
+
+    // Maneja los datos de las publicaciones aquí
+    const posts = response.data;
+
+    // Renderiza la página de inicio y pasa los datos de las publicaciones
+    res.render('/', { posts: posts });
+  } catch (error) {
+    // Maneja los errores
+    console.error('Error al cargar las publicaciones:', error);
+    // Puedes renderizar la página de inicio sin datos o mostrar un mensaje de error
+    res.render('pages/home', { posts: [] });
+  }
 });
 
 // Iniciamos el servidor
---
+++ public/js/script.js
@@ -0,0 +1,89 @@
+// Manejador de evento para el formulario de inicio de sesión
+document.getElementById('login-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    const loginUsername = document.getElementById('username').value;
+    const loginPassword = document.getElementById('password').value;
+  
+    // Datos de inicio de sesión
+    const loginData = {
+      username: loginUsername,
+      password: loginPassword,
+    };
+    
+    // Realizamos la solicitud POST al servidor utilizando Axios para el inicio de sesión
+    axios.post('http://localhost:5000/login', loginData)
+      .then(response => {
+        // Manejamos la respuesta del servidor
+        if (response.data) {
+          // Inicio de sesión exitoso
+          console.log('Inicio de sesión exitoso');
+        } else {
+          // Inicio de sesión fallido
+          console.error('Inicio de sesión fallido');
+        }
+      })
+      .catch(error => {
+        // Manejamos los errores
+        console.error('Error:', error);
+      });
+  });
+  
+  // Manejador de evento para el formulario de registro
+  document.getElementById('signup-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    const firstname = document.getElementById('firstname').value;
+    const lastname = document.getElementById('lastname').value;
+    const email = document.getElementById('email').value;
+    const signupUsername = document.getElementById('username').value;
+    const signupPassword = document.getElementById('password').value;
+    const profileImage = document.getElementById('profile_image').value;
+  
+    // Datos para la solicitud de registro
+    const signupData = {
+      firstname: firstname,
+      lastname: lastname,
+      email: email,
+      username: signupUsername,
+      password: signupPassword,
+      profile_image: profileImage
+    };
+    
+    // Realiza la solicitud POST al servidor utilizando Axios para el registro
+    axios.post('http://localhost:5000/signup', signupData)
+    console.log(signupData)
+      .then(response => {
+        // Manejamos la respuesta del servidor
+        if (response.data) {
+          // Registro exitoso
+          console.log('Registro exitoso');
+        } else {
+          // Registro fallido
+          console.error('Registro fallido');
+        }
+      })
+      .catch(error => {
+        // Manejamos los errores
+        console.error('Error:', error);
+      });
+  });
+  
+
+  // Agregamos un evento click al enlace "Posts"
+  const postsLink = document.getElementById('posts-link');
+  postsLink.addEventListener('click', function(event) {
+    event.preventDefault(); // Evitamos la navegación predeterminada
+    
+    // Realizamos la solicitud Axios cuando se hace clic en "Posts"
+    axios.get('http://localhost:5000/posts')
+      .then(response => {
+        // Manejamos la respuesta aquí, por ejemplo, puedes mostrar los datos en la página o redirigir a una nueva página.
+        console.log('Respuesta de Axios:', response.data);
+      })
+      .catch(error => {
+        console.error('Error en la solicitud Axios:', error);
+      });
+  });
+
+  
